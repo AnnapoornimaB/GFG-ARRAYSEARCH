99306f00ed2524adf4b76917414a881500d4213f
AnnapoornimaB <157682950+AnnapoornimaB@users.noreply.github.com>
2024-06-22 00:33:58 +0530

    Create GFG.java
    Print alternate elements of an array (GFG)

---
+++ GFG.java
@@ -0,0 +1,11 @@
+//Print alternate elements of an array - Geeks for Geeks Solution
+class GfG
+{
+    public static void print(int arr[], int n)
+    {
+        for(int i=0;i<n;i+=2)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
